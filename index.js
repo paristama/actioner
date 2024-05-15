5a12a2e0d94f767ae6da6b0232a5fe638ed5cfc7
Paris Tama <parisxtama@gmail.com>
2024-05-15 13:50:09 +0700

    add moment-timezone js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import fs from "fs";
+import moment from "moment-timezone";
+
+moment.tz.setDefault("Asia/Jakarta").locale("id");
 
 (function () {
   if (!fs.existsSync("log.txt")) {
@@ -6,7 +9,13 @@ import fs from "fs";
   }
 
   const logText = fs.readFileSync("log.txt")?.toString();
-  const newLog = new Date().toLocaleString("id-ID") + "\n" + logText;
+  const date = new Date();
+  const newLog =
+    moment(date).format("ddd, D MMM YYYY HH:mm:ss") +
+    " | " +
+    date.toLocaleString("id-ID") +
+    "\n" +
+    logText;
   fs.writeFileSync("log.txt", newLog);
   console.log(newLog);
 })();
